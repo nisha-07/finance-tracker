7286e68f8763b4999194966049831b2d4e6a8931
nisha-07 <nishasataki@gmail.com>
2022-12-16 15:29:29 +0530

    FT: Add routes for three pages

--- finance-tracker/src/App.js
+++ finance-tracker/src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
 
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+
 function App() {
   return (
     <div className="App">
-      <h3>Welcome to finance tracker!!!</h3>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" element={<Home />}>
+            <Home />
+          </Route>
+          <Route path="/login" element={<Login />}>
+            <Login />
+          </Route>
+          <Route path="/signup" element={<Signup />}>
+            <Signup />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+
     </div>
   );
 }
