22463e8f3e92722dcf0f4fd168338d60893bd431
nisha-07 <nishasataki@gmail.com>
2022-12-17 15:30:53 +0530

    FT: Create a form for signup page

--- finance-tracker/src/pages/signup/Signup.js
+++ finance-tracker/src/pages/signup/Signup.js
@@ -1,9 +1,26 @@
 import classes from "./Signup.module.css";
+import { useState } from "react";
 
 const Signup = () => {
+    const [name, setName] = useState(null)
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+
     return (
-        <div className="signup">
-            Signup page
+        <div className={classes.signup}>
+            <h3 className="d-flex justify-content-center">Signup</h3>
+            <form>
+                <label>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name"></input>
+                </label>
+                <label>
+                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email"></input>
+                </label>
+                <label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password"></input>
+                </label>
+                <button className={classes.btn}>Signup</button>
+            </form>
         </div>
     )
 }
