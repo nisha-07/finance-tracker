5282914cd05db04290ead1a49910e652933a01db
nisha-07 <nishasataki@gmail.com>
2022-12-17 15:25:06 +0530

    FT: Create a login form

--- finance-tracker/src/pages/login/Login.js
+++ finance-tracker/src/pages/login/Login.js
@@ -1,9 +1,25 @@
 import classes from "./Login.module.css";
+import { useState } from "react";
 
 const Login = () => {
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+
     return (
-        <div className="login">
-            Login page
+        <div className={classes.login}>
+            <h3 className="d-flex justify-content-center">Login</h3>
+            <form>
+                <label>
+                    <span>Email</span>
+                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+                </label>
+                <label>
+                    <span>Password</span>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+                </label>
+                <button className={classes.btn}>Login</button>
+            </form>
+
         </div>
     )
 }
