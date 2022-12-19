8bc9d2e9deceaefa56b1146038fb07b53948a772
nisha-07 <nishasataki@gmail.com>
2022-12-19 10:42:58 +0530

    FT: Conitional links on navbar depend on logged in or not
    Route guarding depend on logged in user or not

--- finance-tracker/src/App.js
+++ finance-tracker/src/App.js
@@ -1,30 +1,34 @@
 import './App.css';
 
-import { BrowserRouter, Route, Switch } from "react-router-dom"
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom"
 
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Navbar from './components/Navbar/Navbar';
 import Signup from './pages/signup/Signup';
+import useAuthContext from './hooks/useAuthContext';
 
 function App() {
+  const { authIsReady, user } = useAuthContext()
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" element={<Home />}>
-            <Home />
-          </Route>
-          <Route path="/login" element={<Login />}>
-            <Login />
-          </Route>
-          <Route path="/signup" element={<Signup />}>
-            <Signup />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" element={<Home />}>
+              {user ? <Home /> : <Redirect to="/login" />}
+            </Route>
+            <Route path="/login" element={<Login />}>
+              {!user ? <Login /> : <Redirect to="/" />}
+            </Route>
+            <Route path="/signup" element={<Signup />}>
+              {!user ? <Signup /> : <Redirect to="/" />}
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
--- finance-tracker/src/components/Navbar/Navbar.js
+++ finance-tracker/src/components/Navbar/Navbar.js
@@ -1,18 +1,26 @@
 import { Link } from "react-router-dom";
 import classes from "./Navbar.module.css";
+import useAuthContext from "../../hooks/useAuthContext";
 import useLogout from "../../hooks/useLogout";
 
 const Navbar = () => {
     const { logout } = useLogout()
+    const { user } = useAuthContext()
 
     return (
         <div className={classes.navbar}>
             <h5 className="ms-4">My Money</h5>
-            <div>
-                <Link to="/signup">Sign up</Link>
-                <Link to="/login">Login</Link>
-                <button onClick={logout}>Log out</button>
-            </div>
+            {!user ?
+                <div>
+                    <Link to="/signup">Sign up</Link>
+                    <Link to="/login">Login</Link>
+                </div> :
+                (<>
+                    <em>Hello, {user.displayName}</em>
+                    <button onClick={logout}>Log out</button>
+                </>)
+            }
+
         </div>
     )
 }
