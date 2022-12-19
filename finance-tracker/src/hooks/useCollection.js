aca1f8b5b21938e31c6380910f4ce3c315a96a33
nisha-07 <nishasataki@gmail.com>
2022-12-19 18:07:44 +0530

    FT: Create a hook to list down transactions

---
+++ finance-tracker/src/hooks/useCollection.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+import { projectFirestore } from "../firebase/config"
+
+export const useCollection = (transaction) => {
+    const [documents, setDocuments] = useState(null)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        let ref = projectFirestore.collection(transaction)
+
+        const unsubscribe = ref.onSnapshot(snapshot => {
+            let results = []
+            snapshot.docs.forEach(doc => {
+                results.push({ ...doc.data(), id: doc.id })
+            });
+
+            // update state
+            setDocuments(results)
+            setError(null)
+        }, error => {
+            console.log(error)
+            setError('could not fetch the data')
+        })
+
+        // unsubscribe on unmount
+        return () => unsubscribe()
+
+    }, [transaction])
+
+    return { documents, error }
+}
