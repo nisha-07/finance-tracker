17c8109044c11289d0aa3558336193ce046b3657
nisha-07 <nishasataki@gmail.com>
2022-12-19 18:08:10 +0530

    FT: Use of hooks to add,delete,view transactions

--- finance-tracker/src/pages/home/Home.js
+++ finance-tracker/src/pages/home/Home.js
@@ -1,18 +1,46 @@
+import { useEffect, useState } from "react";
+
 import classes from "./Home.module.css";
-import { useState } from "react";
+import useAuthContext from "../../hooks/useAuthContext";
+import { useCollection } from "../../hooks/useCollection";
+import useFirestore from "../../hooks/useFirestore";
 
 const Home = () => {
     const [name, setName] = useState("")
     const [amount, setAmount] = useState("")
+    const { addDocument, deleteDocument, response } = useFirestore("transactions")
+    const { user } = useAuthContext()
+    const { documents: transactions, error } = useCollection("transactions")
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        addDocument({
+            uid: user.uid,
+            name, amount
+        })
     }
+
+    useEffect(() => {
+        if (response.success) {
+            setName('')
+            setAmount('')
+        }
+    }, [response.success])
 
     return (
         <div className={classes.home}>
             <div className={classes.transactionList}>
                 <h5 className="mt-5">Transaction list</h5>
+                <ul className={classes.transactions}>
+                    {transactions && transactions.map((transaction) => (
+                        <li key={transaction.id}>
+                            <p className={classes.name}>{transaction.name}</p>
+                            <p className={classes.amount}>${transaction.amount}</p>
+                            <button className={classes.btn} onClick={() => deleteDocument(transaction.id)}>Delete</button>
+                        </li>
+                    ))}
+                </ul>
+                {error && <em>{error}</em>}
             </div>
             <div>
                 <h5 className="mt-5">Add transaction</h5>
