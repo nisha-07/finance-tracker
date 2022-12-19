f7f25abb8843ea944a3595e13b8b16732622389b
nisha-07 <nishasataki@gmail.com>
2022-12-19 11:34:47 +0530

    FT: Create home page and styling it

--- finance-tracker/src/pages/home/Home.js
+++ finance-tracker/src/pages/home/Home.js
@@ -1,9 +1,33 @@
 import classes from "./Home.module.css";
+import { useState } from "react";
 
 const Home = () => {
+    const [name, setName] = useState("")
+    const [amount, setAmount] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <div className="home">
-            Home page
+        <div className={classes.home}>
+            <div className={classes.transactionList}>
+                <h5 className="mt-5">Transaction list</h5>
+            </div>
+            <div>
+                <h5 className="mt-5">Add transaction</h5>
+                <form onSubmit={handleSubmit} className={classes.transactionForm}>
+                    <label>
+                        <span>Transaction name:</span>
+                        <input type="text" value={name} onChange={(e) => setName(e.target.value)}></input>
+                    </label>
+                    <label>
+                        <span>Amount ($):</span>
+                        <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)}></input>
+                    </label>
+                    <button className={classes.btn}>Add transaction</button>
+                </form>
+            </div>
         </div>
     )
 }
